3dd688e38e3425a8aafc5480ccd125367126e39d
Becky Hassler <beckyjhassler@gmail.com>
2022-07-23 14:57:31 -0400

    Try to change product hero to cat img

--- header.php
+++ header.php
@@ -17,9 +17,58 @@
 
 <body <?php body_class(); ?> > 
 
-    <header>
+<?php if(is_product()){ ?>
+
+//custom header would go here
+
+      <div class="container">
+            
+            <!-- Header Background Img -->
+
+            <?php global $wp_query;
+            $cat = $wp_query->get_queried_object();
+            $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); 
+            $background_image = wp_get_attachment_url( $thumbnail_id ); ?>
+
+            
+            <div class="row" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
+
+            
+               	<!-- Logo Icon for Header -->
+                   <div class="col-lg-6 logo-container">
+                    <a href="<?php echo get_home_url(); ?>"><img class="logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Company Logo"/></a>
+                </div>
+
+
+                <!-- Menu for Header -->
+                <div class="col-lg-6 navigation">
+                        <nav class="custom-menu-class">
+                            <?php
+                                wp_nav_menu(array(
+                                    'theme_location' => 'main-menu'
+                                ));
+                            ?>
+                        </nav>
+                </div>
+                
+                <!-- Title of Page -->
+                <div class="div">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+                </div>     
+                
+
+            </div>
+        </div>
+    </header>
+
+  <?php }else{ ?>
+
+  //copy and paste the parent theme header here.
+
+  <header>
         <div class="container">
             
+            <!-- Header Background Img -->
             <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post ->ID), 'full') ;?>
             
             <div class="row" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
@@ -51,3 +100,5 @@
             </div>
         </div>
     </header>
+
+ <?php }?>
